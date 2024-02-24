c3ad59d273ac5350a30402ab30b4de5ea0d13b9c
itzmhizterlouis <54844286+itzmhizterlouis@users.noreply.github.com>
2024-02-24 12:57:37 +0100

    Binary Search Algorithm Implementation (#2)
    * Set up for Data structures and algorithms
    
    * Binary Search Algorithm

--- DSA/src/Main.java
+++ DSA/src/Main.java
@@ -1,5 +1,13 @@
+import datatypes.Numbers;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        int[] list = new int[10000000];
+
+        for (int i = 0;i< 10000000;i++){
+            list[i] = i + 1;
+        }
+        Numbers numbers = new Numbers(list);
+        System.out.println(numbers.search(10000000));
+        System.out.println(numbers.normalSearch(10000000));
 }
